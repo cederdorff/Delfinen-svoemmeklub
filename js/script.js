480f403437d2b0c24d4fc05bcd09fc31287c7c05
Jon <lundbyjon@hotmail.com>
2023-05-10 16:24:21 +0200

    get/fetch json object and transform to array with preparedata

--- js/script.js
+++ js/script.js
@@ -1,10 +1,20 @@
 "use strict";
 
 import { initViews } from "./view-router.js";
+import { getMembers } from "./rest-data.js"
+
+let members;
 
 window.addEventListener("load", startApp);
 
 function startApp() {
   console.log("app is running");
   initViews();
+
+  updateMembersTable();
+}
+
+async function updateMembersTable() {
+  members = await getMembers();
+  console.log(members);
 }
