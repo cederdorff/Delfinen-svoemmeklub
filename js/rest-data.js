e5e3407bf9a6a7be8913efa82ac25b329a74060f
Jon <lundbyjon@hotmail.com>
2023-05-11 10:19:24 +0200

    get/fetch results added

--- js/rest-data.js
+++ js/rest-data.js
@@ -6,13 +6,21 @@ const endpoint = "https://crud-1st-semester-projekt-default-rtdb.firebaseio.com/
 
 async function getMembers() {
   const response = await fetch(`${endpoint}/members.json`);
-  if (response.ok) {
-    console.log("response is ok!");
-  }
   const data = await response.json();
 
   const members = prepareMembersData(data);
   return members;
 }
 
-export { getMembers };
+async function getResults() {
+  const response = await fetch(`${endpoint}/results.json`);
+  if (response.ok) {
+    console.log("response is ok!");
+  }
+  const data = await response.json();
+
+  const results = prepareMembersData(data);
+  return results;
+}
+
+export { getMembers, getResults };
--- js/script.js
+++ js/script.js
@@ -2,27 +2,26 @@
 
 import { initViews } from "./view-router.js";
 import { addThreeNewLinksToNavBar } from "./dom.js";
-import { getMembers } from "./rest-data.js";
+import { getMembers, getResults } from "./rest-data.js";
 
 let members;
+let results;
 
 window.addEventListener("load", startApp);
 
 function startApp() {
   console.log("app is running");
   initViews();
+  updateMembersTable();
 
   document.querySelector("#login-btn").addEventListener("click", loginClicked);
-  document
-    .querySelector("#logon-btn")
-    .addEventListener("click", loginInLoginClicked);
+  document.querySelector("#logon-btn").addEventListener("click", loginInLoginClicked);
 }
 
 function loginClicked() {
   document.querySelector("#login").showModal();
   document.querySelector("#login").scrollTop = 0;
 
-  updateMembersTable();
 }
 
 function loginInLoginClicked() {
@@ -31,5 +30,7 @@ function loginInLoginClicked() {
 }
 async function updateMembersTable() {
   members = await getMembers();
+  results = await getResults();
   console.log(members);
+  console.log(results);
 }
