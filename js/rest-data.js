8a0ac1b892e362650bd337c5d7166dcb109fdd97
MudD3RTv <markusolsen2004@gmail.com>
2023-05-11 09:10:50 +0200

    Tilføjet firebase database link

--- js/rest-data.js
+++ js/rest-data.js
@@ -1,17 +1,17 @@
 import { prepareMembersData } from "./helpers.js";
 
 // ========== Global endpoint variable til firebase========== //
-//const endpoint = "https://movies-forms-rest-crud-afl-default-rtdb.europe-west1.firebasedatabase.app/";
-const endpoint = "../members";
+const endpoint = "https://crud-1st-semester-projekt-default-rtdb.firebaseio.com/";
+// const endpoint = "../members";
 
 async function getMembers() {
-  const response = await fetch(`${endpoint}.json`);
+  const response = await fetch(`${endpoint}/members.json`);
   if (response.ok) {
-    console.log("response is ok!")
+    console.log("response is ok!");
   }
   const data = await response.json();
 
-  const members = prepareMembersData(data);  
+  const members = prepareMembersData(data);
   return members;
 }
 
