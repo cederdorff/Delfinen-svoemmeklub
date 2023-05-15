0ee0e06cd3ba2f8fc20f3f85b923427a174cf7d3
MarkusIngerslev <markusolsen2004@gmail.com>
2023-05-15 21:20:11 +0200

    Fået top5 til at virke(semi), men ikke fremvist

--- js/coach.js
+++ js/coach.js
@@ -1,5 +1,5 @@
 import { getMembersCoach, getMembers } from "./rest-data.js";
-import { results } from "./script.js";
+import { results, members } from "./script.js";
 
 // ----- global variable ----- //
 let membersFiltered;
@@ -10,9 +10,9 @@ async function showCompetitiveMembers(results) {
   // document.querySelector("#sortBy-for-coach").addEventListener("change", sortByForCoach);
 
   // event listener til filtre
-  // document.querySelector("#coachFilterTop5").addEventListener("change", filterforCoach);
-  // document.querySelector("#coachFilterJunior").addEventListener("change", filterforCoach);
-  // document.querySelector("#coachFilterSenior").addEventListener("change", filterforCoach);
+  document.querySelector("#coachFilterTop5").addEventListener("change", filterforCoach);
+  document.querySelector("#coachFilterJunior").addEventListener("change", filterforCoach);
+  document.querySelector("#coachFilterSenior").addEventListener("change", filterforCoach);
 
   showCompetitiveMemberLoop(results);
 }
@@ -44,5 +44,37 @@ async function showCompetitiveMember(memberObject) {
 }
 
 // ========== Sort ========== //
+
+async function filterforCoach() {
+  let filterList = members;
+  const top5 = document.querySelector("#coachFilterTop5");
+  const junior = document.querySelector("#coachFilterJunior");
+  const senior = document.querySelector("#coachFilterSenior");
+
+  if (junior.checked) {
+    filterList = members.filter(isJunior);
+    console.log(filterList);
+  } else if (senior.checked) {
+    filterList = members.filter(isSenior);
+    console.log(filterList);
+  } else if (top5.checked) {
+    filterList = results.sort(top5Results);
+    filterList.splice(5);
+    console.log(filterList);
+  }
+}
+
+function top5Results(result1, result2) {
+  if (results.tournament == false) {
+    return result1.timeMiliSeconds - result2.timeMiliSeconds;
+  }
+}
+
+function isJunior(result) {
+  return result.age < 18 && result.activityForm === "konkurrence-svømmer";
+}
+function isSenior(result) {
+  return result.age >= 18 && result.activityForm === "konkurrence-svømmer";
+}
 
 export { showCompetitiveMembers };
