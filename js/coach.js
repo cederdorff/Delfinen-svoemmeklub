090d756e60ce48f38a605ff4bc79fd3f14a88b13
MarkusIngerslev <markusolsen2004@gmail.com>
2023-05-15 19:48:29 +0200

    Ryttet op i coach kode

--- js/coach.js
+++ js/coach.js
@@ -1,10 +1,24 @@
 import { getMembersCoach } from "./rest-data.js";
+import { results } from "./script.js";
+
+// ----- global variable ----- //
+let membersFiltered;
+let isFilterOn;
 
 async function showCompetitiveMembers(results) {
-  // createTable();
-
   // event listener til sort
-  document.querySelector("#sortBy-for-coach").addEventListener("change", sortByForCoach);
+  // document.querySelector("#sortBy-for-coach").addEventListener("change", sortByForCoach);
+
+  // event listener til filtre
+  document.querySelector("#coachFilterTop5").addEventListener("change", filterforCoach);
+  document.querySelector("#coachFilterJunior").addEventListener("change", filterforCoach);
+  document.querySelector("#coachFilterSenior").addEventListener("change", filterforCoach);
+
+  showCompetitiveMemberLoop(results);
+}
+
+function showCompetitiveMemberLoop(results) {
+  document.querySelector("#coach-members-tbody").innerHTML = "";
 
   for (const result of results) {
     if (result.tournament === false) {
@@ -13,32 +27,11 @@ async function showCompetitiveMembers(results) {
   }
 }
 
-function createTable() {
-  document.querySelector("#for-coach-article").insertAdjacentHTML(
-    "beforeend",
-    /*html*/ `
-    <h2>Atleter</h2>
-    <table id="for-coach-table-junior">
-    <thead>
-      <tr>
-        <th>Navn</th>
-        <th>Alder</th>
-        <th>Disciplin(er)</th>
-        <th>Tider</th>
-        <th>Dato</th>
-      </tr>
-    </thead>
-    </table>
-    `
-  );
-}
-
 async function showCompetitiveMember(memberObject) {
   const member = await getMembersCoach(memberObject.memberId);
   document.querySelector("#coach-members-tbody").insertAdjacentHTML(
     "beforeend",
     /*html*/ `
-    <tbody>
       <tr>
         <td>${member.firstname + " " + member.lastname}</td>
         <td>${member.age}</td>
@@ -46,7 +39,6 @@ async function showCompetitiveMember(memberObject) {
         <td>${memberObject.timeMiliSeconds}ms</td>
         <td>${memberObject.date}</td>
       </tr>
-    </tbody>
     `
   );
 }
@@ -55,6 +47,26 @@ async function showCompetitiveMember(memberObject) {
 
 function sortByForCoach(event) {
   const value = event.target.value;
+}
+
+async function filterforCoach() {
+  const top5 = document.querySelector("#coachFilterTop5");
+  const junior = document.querySelector("#coachFilterJunior");
+  const senior = document.querySelector("#coachFilterSenior");
+
+  if (top5.checked || junior.checked || senior.checked) {
+    membersFiltered = results.filter(checkFilter);
+    isFilterOn = true;
+    showCompetitiveMemberLoop(results);
+  } else {
+    isFilterOn = false;
+    showCompetitiveMemberLoop(results);
+  }
+
+  function checkFilter(result) {
+    if (junior.checked) {
+    }
+  }
 }
 
 export { showCompetitiveMembers };
