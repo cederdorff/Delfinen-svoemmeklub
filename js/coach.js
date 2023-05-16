91e9ccc9f02210d2279657c07f53f0d5c0c53c3a
MarkusIngerslev <markusolsen2004@gmail.com>
2023-05-16 15:47:52 +0200

    added sådan at sort-by selekter nu virker sammen med filtre

--- js/coach.js
+++ js/coach.js
@@ -1,13 +1,16 @@
-import { getMembersCoach, getMembers } from "./rest-data.js";
-import { results, members } from "./script.js";
+import { getMembersCoach } from "./rest-data.js";
+import { results, updateMembersTable } from "./script.js";
 
 // ----- global variable ----- //
-let membersFiltered;
+
+let filterList;
 let isFilterOn;
+
+// ========== show competitve members ========== //
 
 async function showCompetitiveMembers(results) {
   // event listener til sort
-  // document.querySelector("#sortBy-for-coach").addEventListener("change", sortByForCoach);
+  document.querySelector("#sortBy-for-coach").addEventListener("change", sortByForCoach);
 
   // event listener til filtre
   document.querySelector("#coachFilterTop5").addEventListener("change", filterforCoach);
@@ -50,35 +53,64 @@ async function showCompetitiveMember(memberObject) {
 }
 
 // ========== Sort ========== //
+function sortByForCoach(event) {
+  const value = event.target.value;
 
+  if (value === "none") {
+    updateMembersTable();
+  } else if (value === "age" && !isFilterOn) {
+    results.sort(compareAge);
+    showCompetitiveMemberLoop(results);
+  } else if (value === "age" && isFilterOn) {
+    filterList.sort(compareAge);
+    showCompetitiveMemberLoop(filterList);
+  } else if (value === "time" && !isFilterOn) {
+    results.sort(compareTime);
+    showCompetitiveMemberLoop(results);
+  } else if (value === "time" && isFilterOn) {
+    filterList.sort(compareTime);
+    showCompetitiveMemberLoop(filterList);
+  }
+
+  function compareAge(result1, result2) {
+    return result1.member.age - result2.member.age;
+  }
+
+  function compareTime(result1, result2) {
+    return result1.timeMiliSeconds - result2.timeMiliSeconds;
+  }
+}
+
+// ========== filter ========== //
 async function filterforCoach() {
-  let filterList = results;
   const top5 = document.querySelector("#coachFilterTop5");
   const junior = document.querySelector("#coachFilterJunior");
   const senior = document.querySelector("#coachFilterSenior");
 
   if (junior.checked) {
     filterList = results.filter(isJunior);
+    isFilterOn = true;
     console.log(filterList);
     showCompetitiveMemberLoop(filterList);
   } else if (senior.checked) {
     filterList = results.filter(isSenior);
+    isFilterOn = true;
     console.log(filterList);
     showCompetitiveMemberLoop(filterList);
   } else if (top5.checked) {
     filterList = results.sort(top5Results);
-    // filterList.splice(5);
+    isFilterOn = true;
     console.log(filterList);
     showCompetitiveMemberLoop(filterList);
   } else {
+    filterList = results;
+    isFilterOn = false;
     showCompetitiveMemberLoop(results);
   }
 }
 
 function top5Results(result1, result2) {
-  if (results.tournament == false) {
-    return result1.timeMiliSeconds - result2.timeMiliSeconds;
-  }
+  return result1.timeMiliSeconds - result2.timeMiliSeconds;
 }
 
 function isJunior(result) {
